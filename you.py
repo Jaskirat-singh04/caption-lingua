f1762bccad04a2c81f046e11ca9f71f2a4679496
Jaskirat-singh04 <83869412+Jaskirat-singh04@users.noreply.github.com>
2023-04-10 00:42:17 +0530

    Update you.py

--- you.py
+++ you.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from youtube_transcript_api import YouTubeTranscriptApi
 
 # Replace 5713513900:AAE8T2-6M55Q2f_cFP3Cn77jsgxJQoiyxEg with your actual bot token
-bot = telegram.Bot(token='5713513900:AAE8T2-6M55Q2f_cFP3Cn77jsgxJQoiyxEg')
+bot = telegram.Bot(token='5713513900:AAE8T2-6M55Q2f_cFP3Cn77jsgxJQoiyxxx')
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm a YouTube transcript bot. Send me a YouTube video URL and the language you want the transcript in. For example: /transcript https://www.youtube.com/watch?v=dQw4w9WgXcQ en")
